a1662af362ffd1eb41941593a576035bd668b4ad
john <john.d.leonetti@gmail.com>
2021-03-10 13:22:35 -0800

    commit tree b262dd9aebe8f3255d3c2bb98c46f014a02f9ab7

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+class Project 
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def backers
+        @backers
+    end
+
+    def add_backer(backer)
+        @backers << backer
+        back = ProjectBacker.new(self, backer)
+    end
+
+
+
+end
